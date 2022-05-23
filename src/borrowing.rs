ae80bb238a83f5b9193b44f64a751d84814518bc
mandigaaravind <mandigaaravind@gmail.com>
2022-05-23 11:14:39 +0530

    borrow changes

--- src/borrowing.rs
+++ src/borrowing.rs
@@ -1,23 +1,28 @@
-pub fn run() {
-    let mut a=String::from("hello");
-    let b=&a;
-    let c=&a;
-    println!("{}",a);
-    println!("{} {}",b,c);
-    let d= match a.pop(){
-       Some(f)=>f,
-       None=>'4',
-    };
-    println!("{}",a);
-    println!("{}",d);
+fn main() {
+   let mut a=String::from("hello");
+   let b=&a;
+   let c=&a;
+   println!("{}",a);
+   println!("{} {}",b,c);
+   let d= match a.pop(){
+      Some(f)=>f,
+      None=>'4',
+  };
+  println!("{}",a);
+  println!("{}",d);
+  a.pop();
+   println!("{}",a);
+   let e=&mut a;
+   // a.pop(); --doesnt work as e is already in scope.
+  //  println!("{}", a);
+   println!("{}",e);
+   e.pop();
+   println!("{}",e);
+
+   own_and_borrow(&mut a,d);
+   println!("{}",a);
+}
+
+pub fn own_and_borrow(a:&mut String,d:char){
     a.pop();
-    println!("{}",a);
-    let e=&mut a;
- //    a.pop(); --doesnt work as e is already in scope.
-    // println!("{}", a);
-    println!("{}",e);
-    e.pop();
-    println!("{}",e);
- }
- 
- 
+}
